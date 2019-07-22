c15c6050fe2e6e1cacbdee31b38835a34ebf3a8a
wangyuliang <2589475332@qq.com>
2019-07-22 23:28:32 +0800

    登录页

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import { Form, Icon, Input, Button } from 'antd';
 import './login.less'
+import logo from './images/logo.png'
 
 export default class Login  extends Component {
     constructor(props) {
@@ -10,8 +12,33 @@ export default class Login  extends Component {
     render() {
         return (
             <div className="login">
-                <header className="login-header"></header>
-                <section className="login-content"></section>
+                <header className="login-header">
+                    <img src={logo} alt="log"/>
+                    <h1>居然：后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <Form layout="inline" onSubmit={this.handleSubmit}>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" >
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
             </div>
         )
     }
